ac80e9ad6f261fdabd465c70321c5f8d9c0f98b6
rinnshi7 <rinnshi7@gmail.com>
2024-11-25 11:07:25 +0800

    update: update addpost

--- src/app/addpost/page.tsx
+++ src/app/addpost/page.tsx
@@ -1,11 +1,19 @@
 import {PrismaClient} from "@prisma/client";
+import {map} from "yaml/dist/schema/common/map";
 
 const prisma = new PrismaClient()
 
 export default async function Post() {
     const allUsers = await prisma.post.findMany()
     console.log(allUsers)
-    return "hello";
+    return (allUsers.map((user) => {
+        return (
+            <div key={user.id}>
+                <h2>{user.title}</h2>
+                <p>{user.body}</p>
+            </div>
+        );
+    }))
 }
 Post().catch((e) => {
     throw e
